e3f2e601d3fac86836708fa87954fcf2cdebcf56
/Namhyun Cho </mr.cnh9110@gmail.com>
2020-05-31 17:57:47 -0500

    Add procfile and update ports to connect to Heroku

--- app.js
+++ app.js
@@ -429,6 +429,11 @@ app.post("/freight-detail/:_id", function(req, res) {
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on MongoDB Atlas");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
