a90dd7ea4673a2bafc6166b2354421db62722986
c-nam-h <mr.cnh9110@gmail.com>
2020-07-01 00:04:36 -0500

    Requests in the freight report page are sorted in descending order showing the newest ones first.

--- controllers/freightReport.js
+++ controllers/freightReport.js
@@ -2,8 +2,8 @@ const Freight = require("../models/Freight");
 const Request = require("../models/Request");
 
 module.exports = async function(req, res) {
-
     const freights = await Freight.find({});
+
     const requests = await Request.find({});
 
     res.render("freight-report", {
