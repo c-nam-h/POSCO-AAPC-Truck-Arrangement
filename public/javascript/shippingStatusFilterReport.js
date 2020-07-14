8d2167586d129377d6742e9d50ff1111154da44e
c-nam-h <mr.cnh9110@gmail.com>
2020-07-13 22:16:46 -0500

    Freight Report can be filtered by shipping status.

---
+++ public/javascript/shippingStatusFilterReport.js
@@ -0,0 +1,40 @@
+shippingStatusFilter("all");
+
+function shippingStatusFilter(status) {
+    let requests = document.getElementsByTagName("tr");
+    
+    if (status === "all") {
+        status = "";
+    }
+
+    for (var i = 0; i < requests.length; i++) {
+        removeClass(requests[i], "show-tr");
+        if (requests[i].className.indexOf(status) > -1) {
+            addClass(requests[i], "show-tr");
+        }
+    }
+}
+
+function addClass(element, name) {
+    let arr1 = element.className.split(" ");
+    let arr2 = name.split(" ");
+
+    for (var i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) === -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function removeClass(element, name) {
+    let arr1 = element.className.split(" ");
+    let arr2 = name.split(" ");
+
+    for (var i = 0; i < arr1.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
